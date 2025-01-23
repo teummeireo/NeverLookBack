781071f5dad3bd8d286d4638bc44f6f0c010dcf3
Hong Yoongi <hyg4work@gmail.com>
2025-01-23 11:17:28 +0900

    db 정보 변경

--- src/main/java/com/nlb/controller/HomeController.java
+++ src/main/java/com/nlb/controller/HomeController.java
@@ -11,9 +11,7 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 
