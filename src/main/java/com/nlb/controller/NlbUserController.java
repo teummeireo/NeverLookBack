8f35e95d641d6f05356372557f7863aeef43856d
INKWON <dlsrnjs066@gmail.com>
2025-01-29 23:21:28 +0900

    [feat]#39-코드 컨벤션으로 정리 및 AnswerVO 따로 분리

--- src/main/java/com/nlb/controller/NlbUserController.java
+++ src/main/java/com/nlb/controller/NlbUserController.java
@@ -1,8 +1,19 @@
 package com.nlb.controller;
 
 
+import javax.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class NlbUserController {
+
+  @PostMapping("/login")
+  public ResponseEntity<String> login(HttpSession session) {
+    session.setAttribute("userId", 1); // 세션에 userId 저장
+    return new ResponseEntity<>("Login successful", HttpStatus.OK);
+  }
+
 }
