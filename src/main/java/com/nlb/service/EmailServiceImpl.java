b97303d0e7dd1eecd1ed506bc7fa402450bb5430
Hong Yoongi <hyg4work@gmail.com>
2025-02-04 17:23:43 +0900

    [feat] #75 로그인 로그아웃 구현 완료

--- src/main/java/com/nlb/service/EmailServiceImpl.java
+++ src/main/java/com/nlb/service/EmailServiceImpl.java
@@ -24,7 +24,23 @@ public class EmailServiceImpl implements EmailService {
 
         // 이메일 내용 작성
         String subject = "NeverLookBack 회원가입 이메일 인증 확인";
-        String body = "<h2>이메일 인증 코드</h2><p>아래의 인증 코드를 입력하세요.</p><h3>" + verificationCode + "</h3>";
+        String body = "<div style='width:100%;height:100%;color:#333'>"
+                + "<div style='border:1px solid #ccc;width:730px;height:800px;margin:50px auto;border-radius:10px'>"
+                + "<div style='color:#000000;margin:0 50px;font-size:20px;text-align:left;margin-top:60px'>"
+                + "<h3>안녕하세요, 고객님.</h3>"
+                + "</div>"
+                + "<div style='color:#000000;margin:0 50px;font-size:19px;text-align:left;margin-top:30px'>NeverLookBack을 찾아주셔서 감사합니다! 인증 코드는</div>"
+                + "<div style='color:#0052a9;font-size:30px;margin-top:30px;text-align:center'>"
+                + "<h3>" + verificationCode + "</h3>"
+                + "</div>"
+                + "<div style='color:#000000;margin:0 50px;font-size:19px;text-align:left;margin-top:50px'>이며, 10분 내에 입력해 주시면 감사하겠습니다.</div>"
+                + "<div style='margin:0 50px;font-size:20px;text-align:left;margin-top:80px'><img src=\"https://github.com/user-attachments/assets/e603a0e3-3d4f-464b-8f76-286ce6221785\" style=\"height:76px\">"
+                + "<div style='border-bottom:2px dashed #000;margin-top:15px'></div>"
+                + "</div>"
+                + "<div style='color:#808080;margin:0 50px;font-size:19px;text-align:left;margin-top:17px'>타인과 인증 코드를 공유하지 말아 주세요."
+                + "<br>시스템 메세지입니다, 해당 메일로 답장을 보내지 말아 주세요."
+                + "</div>"
+                + "</div>";
 
         // 실제 이메일 전송
         emailSender.sendEmail(email, subject, body);
